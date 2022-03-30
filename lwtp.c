5b34ea2c6c222e1fd61e49273cb85c02fb8c8feb
WillMerges <wdmerges@gmail.com>
2022-03-30 00:03:16 -0400

    lots of problems, try again later

--- lwtp.c
+++ lwtp.c
@@ -8,14 +8,12 @@ typedef void (*job_handler_t)(void*);
 typedef struct {
     pthread_t* threads;
     int num_threads;
-    // _Atomic (int) count;
     int count;
     pthread_mutex_t mutex;
     pthread_cond_t cond;
     pthread_mutex_t cond_mutex;
     job_handler_t job;
     void* arg;
-    pthread_mutex_t block;
 } lwt_pool_t;
 
 #include "lwtp.h"
@@ -28,10 +26,8 @@ void* _worker(void* pool_p) {
     void* arg;
 
     while(1) {
-        // keep this locked whenever a worker is waiting
-        pthread_mutex_lock(&(pool->block));
-
         // wait for a job
+        // TODO problem - next thread may not wake up in time before another job is started
         pthread_mutex_lock(&(pool->cond_mutex));
         pthread_cond_wait(&(pool->cond), &(pool->cond_mutex));
 
@@ -46,9 +42,6 @@ void* _worker(void* pool_p) {
 
         pthread_mutex_unlock(&(pool->mutex));
 
-        // unblock whoever gave us the job to let them know we took it
-        pthread_mutex_unlock(&(pool->block));
-
         // execute the job
         job(arg);
 
@@ -60,26 +53,17 @@ void* _worker(void* pool_p) {
 
 int lwtp_start(lwt_pool_t* pool, job_handler_t job, void* arg) {
     pthread_mutex_lock(&(pool->mutex));
+    if(pool->count == pool->num_threads) {
+        // no workers available
+        pthread_mutex_unlock(&(pool->mutex));
+        return -1;
+    }
+
     pool->job = job;
     pool->arg = arg;
     pthread_mutex_unlock(&(pool->mutex));
 
     pthread_cond_signal(&(pool->cond));
-
-    // there's 2 cases here
-    // we block until the worker unlocks this OR
-    // we get it immediately because there was no worker available
-    pthread_mutex_lock(&(pool->block));
-
-    // if there was no worker available, the job will not be taken and we should return error
-    pthread_mutex_lock(&(pool->mutex));
-    job_handler_t job_cp = pool->job;
-    pthread_mutex_unlock(&(pool->mutex));
-
-    if(job_cp) {
-        // no one took the job :(
-        return -1;
-    }
 
     return 0;
 }
@@ -92,6 +76,9 @@ void lwtp_wait_count(lwt_pool_t* pool, int count) {
     while(pool->count >= count) {};
 }
 
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t cond_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 int lwtp_create(lwt_pool_t* pool, int num_threads) {
     if(pthread_mutex_init(&(pool->mutex), NULL)) {
         // failed to initialize mutex
@@ -99,11 +86,6 @@ int lwtp_create(lwt_pool_t* pool, int num_threads) {
     }
 
     if(pthread_mutex_init(&(pool->cond_mutex), NULL)) {
-        // failed to initialize mutex
-        return -1;
-    }
-
-    if(pthread_mutex_init(&(pool->block), NULL)) {
         // failed to initialize mutex
         return -1;
     }
@@ -154,10 +136,6 @@ int lwtp_destroy(lwt_pool_t* pool) {
     }
 
     if(pthread_mutex_destroy(&(pool->cond_mutex))) {
-        return -1;
-    }
-
-    if(pthread_mutex_destroy(&(pool->block))) {
         return -1;
     }
 
--- test.c
+++ test.c
@@ -5,12 +5,16 @@ void test(void* t) {
     printf("job executed\n");
 }
 
+// for some reason this can't go on the stack
+// it may be a compiler optimization seeing the spinning while at the end of main and returning
+lwt_pool_t pool;
+
 int main() {
-    lwt_pool_t pool;
 
-    lwtp_create(&pool, 10);
-
-    // lwtp_start(&pool, &test, NULL);
+    lwtp_create(&pool, 1);
+    
+    int ret = lwtp_start(&pool, &test, NULL);
+    printf("%i\n", ret);
 
     while(1) {};
 }
